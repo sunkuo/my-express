a76535589b8f853fae803e33665616be0318ff2f
sunkuo <11055248@qq.com>
2017-11-16 16:23:30 +0800

    feat($lib): support path of middleware

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,15 @@
 const express = require('../index.js')
 const app = express()
 
-app.get('foo', function (req, res, next) {
+app.get('/foo', function (req, res, next) {
   res.end('Welcome to GET /foo')
 })
 
-app.get('bar', function (req, res, next) {
+app.get('/bar', function (req, res, next) {
   res.end('Welcome to GET /bar')
 })
 
-app.post('foo', function (req, res, next) {
+app.post('/foo', function (req, res, next) {
   res.end('Welcome to POST /foo')
 })
 
